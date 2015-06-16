60fc24d89b7b561e1521814896e5411b641756e4
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-16 05:44:46 -0500

    added function to delete tasks

--- views.py
+++ views.py
@@ -120,3 +120,18 @@ def complete(task_id):
 	g.db.close()
 	flash('The task was marked as complete.')
 	return redirect(url_for('tasks'))
+
+# delete tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+	g.db = connect_db
+	g.db.execute(
+		'delete from tasks where task_id=' + str(task_id)
+		)
+	g.db.commit()
+	g.db.close()
+	flash('The task was deleted.')
+	return redirect(url_for('tasks'))
+
+	
