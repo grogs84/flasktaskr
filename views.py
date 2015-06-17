a0abf1f6055e70b84145e6c6b64b3e4e289f0a7a
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-17 06:23:39 -0500

    fixed add/delete works on chrome

--- views.py
+++ views.py
@@ -126,7 +126,7 @@ def complete(task_id):
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete_entry(task_id):
-	g.db = connect_db
+	g.db = connect_db()
 	g.db.execute(
 		'delete from tasks where task_id=' + str(task_id)
 		)
