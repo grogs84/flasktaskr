70f725d081af26e582891597eec233494cc3aaff
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-22 09:49:10 -0500

    updated login.html pg186 on pdf

--- views.py
+++ views.py
@@ -23,6 +23,11 @@ from models import Task, User
 
 # helper functions
 
+def flash_errors(form):
+	for field, errors in form.errors.items():
+		for error in errors:
+			flash(u"Error in the %s field - %s" % (
+				getattr(form, field).label.text, error), 'error')
 
 
 def login_required(test):
