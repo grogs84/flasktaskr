bb311e8beaadfb8c9fff2a86e8d1546b82ae8c93
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-22 09:09:17 -0500

    added users to sessions

--- views.py
+++ views.py
@@ -57,6 +57,7 @@ def register():
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash("Goodbye!")
 	return redirect(url_for('login'))
 
@@ -70,6 +71,7 @@ def login():
 			user = User.query.filter_by(name=request.form['name']).first()
 			if user is not None and user.password == request.form['password']:
 				session['logged_in'] = True
+				session['user_id'] = user.id
 				flash("Welcome")
 				return redirect(url_for('tasks'))
 			else:
@@ -109,12 +111,17 @@ def new_task():
 				form.priority.data,
 				datetime.datetime.utcnow(),
 				'1',
-				'1',
+				session['user_id'],
 			)
 			db.session.add(new_task)
 			db.session.commit()
 			flash('New entry was successfully posted. Thanks')
-	return redirect(url_for('tasks'))
+			return redirect(url_for('tasks'))
+		else:
+			flash('All fields are required')
+			return redirect(url_for('tasks'))
+	return render_template('tasks.html', form=form)
+
 
 # mark tasks as complete
 @app.route('/complete/<int:task_id>/')
