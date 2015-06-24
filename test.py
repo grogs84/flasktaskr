341c12b46dda5bad5e9fce8e71efe8e11b35ae24
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-24 05:56:21 -0500

    more testing added

--- test.py
+++ test.py
@@ -28,8 +28,13 @@ class AllTests(unittest.TestCase):
 	# helper functions
 	def login(self, name, password):
 		return self.app.post('/', data=dict(
-			name=name, password=password), follow_redirects=True
-		)
+			name=name, password=password), follow_redirects=True)
+
+	def register(self, name, email, password, confirm):
+		return self.app.post(
+			'register/',
+			data=dict(name=name, email=email, password=password, confirm=confirm),
+			follow_redirects=True)
 
 	# each test should start with 'test'
 	def test_user_setup(self):
@@ -49,6 +54,21 @@ class AllTests(unittest.TestCase):
 	def test_users_cannot_login_unless_registered(self):
 		response = self.login('foo', 'bar')
 		self.assertIn(b'Invalid username or password', response.data)
+
+	def test_uses_can_login(self):
+		self.register("Michael", 'michael@realpython.com', 'python', 'python')
+		response = self.login('Michael', 'python')
+		self.assertIn('Add a new task:', response.data)
+
+	def test_invalid_form_data(self):
+		self.register('Michael', 'michael@realpython.com', 'python', 'python')
+		response = self.login('alert("alert box!");', 'foo')
+		self.assertIn(b'Invalid username or password', response.data)
+
+	def test_form_is_present_on_register_page(self):
+		response = self.app.get('register/')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(b'Please register to access the task list', response.data)
 
 
 
