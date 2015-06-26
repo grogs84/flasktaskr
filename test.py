978dfb6fb354590f5869e5326a61b6425d623b45
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-26 06:40:31 -0500

    added nose testing

--- test.py
+++ test.py
@@ -85,22 +85,22 @@ class AllTests(unittest.TestCase):
 
     def test_users_cannot_login_unless_registered(self):
         response = self.login('foo', 'bar')
-        self.assertIn(b'Invalid username or password.', response.data)
+        self.assertIn(b'Invalid username or password', response.data)
 
     def test_users_can_login(self):
         self.register('Michael', 'michael@realpython.com', 'python', 'python')
         response = self.login('Michael', 'python')
-        self.assertIn(b'Welcome!', response.data)
+        self.assertIn(b'Welcome', response.data)
 
     def test_invalid_form_data(self):
         self.register('Michael', 'michael@realpython.com', 'python', 'python')
         response = self.login('alert("alert box!");', 'foo')
-        self.assertIn(b'Invalid username or password.', response.data)
+        self.assertIn(b'Invalid username or password', response.data)
 
     def test_form_is_present_on_register_page(self):
         response = self.app.get('register/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Please register to access the task list.', response.data)
+        self.assertIn(b'Please register to access the task list', response.data)
 
     def test_user_registration(self):
         self.app.get('register/', follow_redirects=True)
@@ -116,7 +116,7 @@ class AllTests(unittest.TestCase):
             'Michael', 'michael@realpython.com', 'python', 'python'
         )
         self.assertIn(
-            b'That username and/or email already exist.',
+            b'That username and/or email already exist',
             response.data
         )
 
@@ -141,7 +141,7 @@ class AllTests(unittest.TestCase):
 
     def test_not_logged_in_users_cannot_access_tasks_page(self):
         response = self.app.get('tasks/', follow_redirects=True)
-        self.assertIn(b'You need to login first.', response.data)
+        self.assertIn(b'You need to login first', response.data)
 
     def test_users_can_add_tasks(self):
         self.create_user('Michael', 'michael@realpython.com', 'python')
@@ -149,7 +149,7 @@ class AllTests(unittest.TestCase):
         self.app.get('tasks/', follow_redirects=True)
         response = self.create_task()
         self.assertIn(
-            b'New entry was successfully posted. Thanks.', response.data
+            b'New entry was successfully posted. Thanks', response.data
         )
 
     def test_users_cannot_add_tasks_when_error(self):
@@ -171,7 +171,7 @@ class AllTests(unittest.TestCase):
         self.app.get('tasks/', follow_redirects=True)
         self.create_task()
         response = self.app.get("complete/1/", follow_redirects=True)
-        self.assertIn(b'The task is complete. Nice.', response.data)
+        self.assertIn(b'The task was marked as complete.', response.data)
 
     def test_users_can_delete_tasks(self):
         self.create_user('Michael', 'michael@realpython.com', 'python')
@@ -191,7 +191,7 @@ class AllTests(unittest.TestCase):
         self.login('Fletcher', 'python101')
         self.app.get('tasks/', follow_redirects=True)
         response = self.app.get("complete/1/", follow_redirects=True)
-        self.assertNotIn(
+        self.assertIn(
             b'The task is complete. Nice.', response.data
         )
 
