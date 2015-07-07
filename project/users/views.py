6aa07b471bd05e1ea991827151f12f835cebe405
Matt Grogan <johnmattgrogan@gmail.com>
2015-07-07 06:31:54 -0500

    addings blueprint to users.views.py

--- project/users/views.py
+++ project/users/views.py
@@ -1,44 +1,29 @@
-# project/views.py
+# project/users/views.py
 
-
+######################
+### imports ##########
+######################
 
 from functools import wraps
 
-from flask import Flask, flash, redirect, render_template, \
-	request, session, url_for, g
+from flask import flash, redirect, render_template, \
+	request, session, url_for, Blueprint
 
-from forms import AddTaskForm, RegisterForm, LoginForm
-from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 
-import datetime
+from .forms import RegisterForm, LoginForm
+from project import db
+from project.models import users
 
+########################
+## config ##############
+## #####################
 
-# config
+users_blueprint = Blueprint('users', __name__)
 
-app = Flask(__name__)
-app.config.from_object('_config')
-db = SQLAlchemy(app)
-
-from models import Task, User
-
-# helper functions
-
-def open_tasks():
-	return db.session.query(Task).filter_by(
-		status='1').order_by(Task.due_date.asc())
-
-def closed_tasks():
-	return db.session.query(Task).filter_by(
-		status='0').order_by(Task.due_date.asc())
-		
-
-def flash_errors(form):
-	for field, errors in form.errors.items():
-		for error in errors:
-			flash(u"Error in the %s field - %s" % (
-				getattr(form, field).label.text, error), 'error')
-
+#########################
+## helper functions #####
+#########################
 
 def login_required(test):
 	@wraps(test)
@@ -46,44 +31,24 @@ def login_required(test):
 		if 'logged_in' in session:
 			return test(*args, **kwargs)
 		else:
-			flash("You need to login first")
-			return redirect(url_for('login'))
+			flash('You need to login first.')
+			return redirect(url_for('users.login'))
 	return wrap
 
+########################
+## routes ##############
+########################
 
-# route handlers
-@app.route('/register/', methods=['GET', 'POST'])
-def register():
-	error = None
-	form = RegisterForm(request.form)
-	if request.method == 'POST':
-		if form.validate_on_submit():
-			new_user = User(
-				form.name.data,
-				form.email.data,
-				form.password.data,
-			)
-			try:
-				db.session.add(new_user)
-				db.session.commit()
-				flash('Thanks for registering. Please login.')
-				return redirect(url_for('login'))
-			except IntegrityError:
-				error = 'That username and/or email already exists.'
-				return render_template('register.html', form=form, error=error)
-	return render_template('register.html', form=form, error=error)
-
-@app.route('/logout/')
+@users_blueprint.route('/logout')
 @login_required
 def logout():
-	session.pop('logged_in', None)
-	session.pop('user_id', None)
+	session.pop('logged_in', True)
+	session.pop('user_id', True)
 	session.pop('role', None)
-	flash("Goodbye!")
-	return redirect(url_for('login'))
+	flash('Goodbye!')
+	return redirect(url_for('users.login'))
 
-
-@app.route('/', methods=['GET', 'POST'])
+@users_blueprint.route('/', methods=['GET', 'POST'])
 def login():
 	error = None
 	form = LoginForm(request.form)
@@ -94,88 +59,9 @@ def login():
 				session['logged_in'] = True
 				session['user_id'] = user.id
 				session['role'] = user.role
-				flash("Welcome")
-				return redirect(url_for('tasks'))
+				flash('Welcome!')
+				return redirect(url_for('tasks.tasks'))
 			else:
-				error = 'Invalid username or password'
-	return render_template('login.html',form=form,error=error)
-
-
-
-@app.route('/tasks/')
-@login_required
-def tasks():
-	return render_template(
-		'tasks.html',
-		form=AddTaskForm(request.form),
-		open_tasks=open_tasks(),
-		closed_tasks=closed_tasks()
-	)
-
-# add new tasks
-@app.route('/add/', methods=['POST'])
-@login_required
-def new_task():
-	error = None
-	form = AddTaskForm(request.form)
-	if request.method == 'POST':
-		if form.validate_on_submit():
-			new_task = Task(
-				form.name.data,
-				form.due_date.data,
-				form.priority.data,
-				datetime.datetime.utcnow(),
-				'1',
-				session['user_id'],
-			)
-			db.session.add(new_task)
-			db.session.commit()
-			flash('New entry was successfully posted. Thanks')
-			return redirect(url_for('tasks'))
-		else:
-			return render_template('tasks.html', 
-							form=form, 
-							error=error,
-							open_tasks=open_tasks(),
-							closed_tasks=closed_tasks()
-			)
-
-	return render_template('tasks.html', 
-							form=form, 
-							error=error,
-							open_tasks=open_tasks(),
-							closed_tasks=closed_tasks() 
-	)
-
-
-
-# mark tasks as complete
-@app.route('/complete/<int:task_id>/')
-@login_required
-def complete(task_id):
-	new_id = task_id
-	task = db.session.query(Task).filter_by(task_id=new_id)
-	if session['user_id'] == task.first().user_id or session['role'] == 'admin':
-		task.update({"status":"0"})
-		db.session.commit()
-		flash('The task was marked as complete.')
-		return redirect(url_for('tasks'))
-	else:
-		flash('You can only update tasks that belong to you.')
-		return redirect(url_for('tasks'))
-
-
-# delete tasks
-@app.route('/delete/<int:task_id>/')
-@login_required
-def delete_entry(task_id):
-	new_id = task_id
-	task = db.session.query(Task).filter_by(task_id=new_id)
-	if session['user_id'] == task.first().user_id or session['role'] == 'admin':
-		task.delete()
-		db.session.commit()
-		flash('The task was deleted. Why not add a new one?')
-		return redirect(url_for('tasks'))
-	else:
-		flash('You can only delete tasks that belong to you.')
-		return redirect(url_for('tasks'))
+				error = 'Invalid username or password.'
+	return render_template('login.html', form=form, error=error)
+	
