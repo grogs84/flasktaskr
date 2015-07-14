cd0a351410c386bb4c802e961537b51c2bc339ff
Matt Grogan <johnmattgrogan@gmail.com>
2015-07-14 06:36:06 -0500

    updated file to push to heroku

--- project/_config.py
+++ project/_config.py
@@ -7,7 +7,7 @@ DATABASE = 'flasktaskr.db'
 CSRF_ENABLED = True
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
-DEBUG = True
+DEBUG = False
 
 # define the full path for the DATABASE
 DATABASE_PATH = os.path.join(basedir, DATABASE)
