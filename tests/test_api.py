5df954f5a695a68269bcc4aef0560d5f7aff5f7e
Matt Grogan <johnmattgrogan@gmail.com>
2015-07-17 06:42:46 -0500

    finished api unit

--- tests/test_api.py
+++ tests/test_api.py
@@ -75,6 +75,21 @@ class APITest(unittest.TestCase):
 		self.assertIn(b'Run around in circles', response.data)
 		self.assertIn(b'Purchase Real Python', response.data)
 
+	def test_resource_end_point_returns_correct_data(self):
+		self.add_tasks()
+		response = self.app.get('api/v1/tasks/2', follow_redirects=True)
+		self.assertEquals(response.status_code, 200)
+		self.assertEquals(response.mimetype, 'application/json')
+		self.assertIn(b'Purchase Real Python', response.data)
+		self.assertNotIn(b'Run around in circles', response.data)
+
+	def test_invalid_resource_endpoint_returns_error(self):
+		self.add_tasks()
+		response = self.app.get('api/v1/tasks/209', follow_redirects=True)
+		self.assertEquals(response.status_code, 404)
+		self.assertEquals(response.mimetype, 'application/json')
+		self.assertIn(b'Element does not exist', response.data)
+
 
 
 	if __name__ == "__main__":
